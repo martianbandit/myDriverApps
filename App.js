24aacb00a553c8055d1a28371fe4567927edd5a7
Jo Paradis <154098629+martianbandit@users.noreply.github.com>
2024-09-15 04:26:14 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,18 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './screens/LoginScreen';
+import Dashboard from './screens/Dashboard';
 
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Dashboard" component={Dashboard} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
